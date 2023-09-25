aa49b9407ab5a908119dd57ba080e58e229a1ee7
PABLO PARODI <pabparodi@gmail.com>
2023-09-25 20:08:27 +0200

    Modificacion visualizacion de puertos

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.core.env.Environment;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +18,8 @@ import com.formacionbdi.springboot.app.productos.models.services.IProductoServic
 @RestController
 public class ProductoController {
 	
-	//@Autowired
-	//private Environment env;
+	@Autowired
+	private Environment env;
 	
 	@Value("${server.port}")
 	private Integer port;
@@ -30,8 +30,8 @@ public class ProductoController {
 	@GetMapping("/listar")
 	public List<Producto> listar(){
 		return iProductoServices.findAll().stream().map(producto ->{
-			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-			producto.setPort(port);
+			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+//			producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
