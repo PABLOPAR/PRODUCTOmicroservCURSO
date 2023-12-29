67d9ce347d8a38885b0f958643b2ad96f91f9b33
PABLOPAR <pabparodi@gmail.com>
2023-12-29 11:04:02 +0100

    PRUEBA

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(SpringbootServicioProductosApplication.class, args);
 	}
 
